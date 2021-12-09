a03723103e46c121c3546f405fa7b002072b9fe1
James Price <jprice@srcinc.com>
2021-12-08 20:06:16 -0500

    Vunit's newer than 4.4.0 should work.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name='vcst',
-      version="0.0.6",
+      version="0.0.8",
       description="VUnit & cocotb smashed Together",
       author="James Price",
       author_email="jpwice100@gmail.com",
       packages=find_packages(),
-      install_requires=["vunit-hdl==4.4.0", "cocotb>=1.5.1,<2.0"]
+      install_requires=["vunit-hdl>=4.4.0", "cocotb>=1.5.1,<2.0"]
      )
