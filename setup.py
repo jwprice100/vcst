eb3b5800668d81d73fba900cbd6c240da39bff6f
Matthew Schiller <mschille@nrao.edu>
2024-02-28 12:54:18 -0500

    Restrict Vunit, and install dependency on cocotb-bus, document need for devel packages

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='vcst',
       author="James Price",
       author_email="jpwice100@gmail.com",
       packages=find_packages(),
-      install_requires=["vunit-hdl>=4.4.0", "cocotb>=1.5.1,<2.0"]
+      install_requires=["vunit-hdl>=4.4.0,<4.7.0", "cocotb>=1.5.1,<2.0","cocotb-bus>=0.2.1"]
      )
